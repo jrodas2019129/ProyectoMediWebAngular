91f5cdce2b82ff7a9575bc2d7cfba624fe4e7662
Jeremi <jrodas-2019129@kinal.edu.gt>
2021-08-03 11:27:20 -0600

    cambio ruta global

--- src/app/servicios/global.service.ts
+++ src/app/servicios/global.service.ts
@@ -1,4 +1,5 @@
 //ruta que mas se repite
 export var GLOBAL = {
-  url: "http://localhost:3000/api/"
+  //url: "http://localhost:3000/api/"
+  url: "https://proyecto-mediweb-node.herokuapp.com/api/"
 }
